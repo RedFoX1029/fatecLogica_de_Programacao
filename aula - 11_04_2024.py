a6fc44eadf57e983a695bea811580f49ff5485c2
RedFoX1029 <138073857+RedFoX1029@users.noreply.github.com>
2024-04-11 10:23:32 -0300

    Add: aula - 11_04_2024
    Atividade realizada em sala de aula

---
+++ aula - 11_04_2024.py
@@ -0,0 +1,24 @@
+'''
+X = int(input())
+Y = float(input())
+KM = X / Y
+print(f"{KM:.3f} km/l")
+'''
+
+'''
+p1 = int(input("Digite um numero de 1 a 10: "))
+while (p1<1)or (p1>10):
+    print("Você digitou um numero invalido, digite novamente ")
+    p1 = int(input("Digite um numero de 1 a 10: "))
+
+acerto = False
+vezes = 0
+
+while acerto == False:
+    p2 = int(input("Adivinhe o numero: "))
+    vezes += 1
+if(p2 == p1):
+    print("\nAcertou!!!")
+    print("Numero de tentativas: ", vezes)
+    acerto = True
+'''

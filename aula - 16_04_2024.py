308ab027072606b367e8311e2c4e839bb33cab0a
RedFoX1029 <138073857+RedFoX1029@users.noreply.github.com>
2024-04-16 10:08:24 -0300

    Add: aula = 16_04_2024
    Atividades realizadas em sala de aula

---
+++ aula - 16_04_2024.py
@@ -0,0 +1,38 @@
+'''
+age = int(input("Digite a idade do nadador: "))
+
+if age >= 5 and age <= 7:
+    print("Categoria: Infantil A")
+elif age >= 8 and age <= 10:
+    print("Categoria: Infantil B")
+elif age >= 11 and age <= 13:
+    print("Categoria: Juvenil A")
+elif age >= 14 and age <= 17:
+    print("Categoria: Juvenil B")
+elif age >= 18:
+    print("Categoria: Adulto")
+'''
+
+'''
+cod = int(input("Digite o código do pedido(1 à 6): "))
+quant = int(input("Digite a quantidade de itens: "))
+valor = 0
+if cod == 1:
+    valor = 8.10 * quant
+    print(f"O valor total do pedido é: R${valor:.2f}")
+elif cod == 2:
+    valor = 11.30 * quant
+    print(f"O valor total do pedido é: R${valor:.2f}")
+elif cod == 3:
+    valor = 15.50 * quant
+    print(f"O valor total do pedido é: R${valor:.2f}")
+elif cod == 4:
+    valor = 13.10 * quant
+    print(f"O valor total do pedido é: R${valor:.2f}")
+elif cod == 5:
+    valor = 14.30 * quant
+    print(f"O valor total do pedido é: R${valor:.2f}")
+elif cod == 6:
+    valor = 5.00 * quant
+    print(f"O valor total do pedido é: R${valor:.2f}")
+'''
